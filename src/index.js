3a8758fb197fcd91f888bff09a87c7d548ab579e
SAIKAN0 <saikan4ik@gmail.com>
2016-11-22 22:31:17 +0300

    meaningless

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const production = 'production';
+const production = process.env.NODE_ENV || 'production';
 export default function({types: t }) {
   return {
     visitor: {
-    	   MemberExpression(path) {
-	          if (path.get("object").matchesPattern("process.env") && (path.node.property.name ==='NODE_ENV' )){
-	          	path.replaceWith(t.valueToNode(production));
-        	  }
-      }
+        MemberExpression(path) {
+            if (path.get("object").matchesPattern("process.env") && (path.node.property.name ==='NODE_ENV' )){
+                path.replaceWith(t.valueToNode(production));
+            }
+        }
     }
   };
 }
